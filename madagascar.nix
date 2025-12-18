add9b1a6378b82d102f4ff8b3e1447a21b734ff7
sacolle <pedro.h.b.colle@gmail.com>
2025-12-18 14:35:18 -0300

    making the opengl pen work, leaving the rest for later

--- madagascar.nix
+++ madagascar.nix
@@ -9,24 +9,28 @@
   python313Packages,
   scons,
   gcc13Stdenv,
-
-
   # Packages for the pens
+  #rpc package
+  libtirpc,
+
+  #ppm
+  netpbm,
 
   libx11, # add the .dev
   libxaw,
-  # dont think i need this, but
-  libgcc,
 
   libtiff,
   gd,
   plplot,
+  libtool,
   ffmpeg,
   cairo,
   libjpeg,
 
   #opengl
   freeglut,
+  libGLU,
+  libGL
   # glui
 
   # autoreconfHook,
@@ -36,6 +40,57 @@ gcc13Stdenv.mkDerivation (finalAttrs:
 let 
     mkLibPaths = libs: builtins.concatStringsSep "," (map (p: "${p}/lib") libs);
     mkIncludePaths = inc: builtins.concatStringsSep " " (map (p: "-isystem ${p}/include") inc);
+    includePkgs = [
+        # rpc
+        libtirpc.dev
+        # ppm
+        netpbm.dev
+        #X11 headers ( )
+        libx11.dev
+        libxaw.dev
+        # OpenGL (X)
+        freeglut.dev
+        libGLU.dev
+        libGL.dev
+        # tiff (X)
+        libtiff.dev
+        # png (X)
+        gd.dev
+        #  ( )
+        plplot
+        libtool
+        #  ( )
+        ffmpeg.dev
+        # png ( )
+        cairo.dev
+        # libjpeg ( )
+        libjpeg.dev
+    ];
+    #subdirectories in the includes that it does not find
+    extraIncludeFlags = builtins.concatStringsSep " " [
+        " -isystem ${plplot.out}/include/plplot"
+        " -isystem ${cairo.dev}/include/cairo"
+        " -isystem ${ffmpeg.dev}/include/libavcodec"
+        " -isystem ${libtirpc.dev}/include/tirpc/rpc"
+        " -isystem ${netpbm.dev}/include/netpbm"
+    ];
+
+    libPkgs = [
+        libtirpc.out
+        netpbm.out
+        libx11.out
+        libxaw.out
+        freeglut.out
+        libGLU.out
+        libGL.out
+        libtiff.out
+        gd.out
+        plplot.out
+        libtool.lib
+        ffmpeg.lib
+        cairo.out
+        libjpeg.out
+    ];
 in
 {
     pname = "madagascar";
@@ -52,33 +107,26 @@ in
 
 
     postUnpack = ''
+        echo ${mkIncludePaths includePkgs}
         echo "removing unwanted directory" $sourceRoot 
         rm -rf $sourceRoot/user
     '';
 
     preConfigure = ''
         configureFlagsArray+=(
-          "CFLAGS=${mkIncludePaths [ libtiff.dev libx11.dev python313 ] }"
-          "LIBPATH=${mkLibPaths [ libtiff.out libx11.out ] }"
+          "CFLAGS=${ (mkIncludePaths includePkgs) + extraIncludeFlags }"
+          "LIBPATH=${ mkLibPaths libPkgs }"
         )
       '';
 
 
-    nativeBuildInputs = [
-        scons
-
-        libtiff.dev
-        libtiff
-    ];
+    nativeBuildInputs = [ ];
 
     buildInputs = [
-        libtiff.dev
-        libtiff
-
         python313
         python313Packages.pip
         scons
-    ];
+    ] ++ includePkgs ++ libPkgs;
 
 
     preBuild = ''
